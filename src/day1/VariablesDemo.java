7a7194e3d6978ce2dbc3a4e37c6fc52176b5a811
hanin-magdy <163678478+hanin-magdy@users.noreply.github.com>
2026-08-16 18:32:47 +0300

    Initial commit

---
+++ src/day1/VariablesDemo.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class VariablesDemo {
+    static void main() {
+//     int x;  //Variable Declarion
+//        x=10; //Variables Initialization
+//        int a =20;
+//        x =30; //Assignment
+
+        int a=10,b=20,c=30;
+
+    }
+}
